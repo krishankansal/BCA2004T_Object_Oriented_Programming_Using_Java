d163d91efe490ff621c6d26fe3607ca941403b8c
krishankansal <krishan.kansal@gmail.com>
2026-04-07 14:18:28 +0530

    committed on 07-04-2026

--- Unit-III/Programs/Array1.java
+++ Unit-III/Programs/Array1.java
@@ -1,3 +1,4 @@
+// To print elements of an array using for loop
 class Array1{
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
---
+++ Unit-III/StringProg/D.java
@@ -0,0 +1,13 @@
+class D {
+    public static void main(String[] args) {
+        String s = "Java Programming";
+        System.out.println(s);
+        char c = s.charAt(0);
+        System.out.println(c);
+        System.out.println(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print(s.charAt(i) + " ");
+        }
+
+    }
+}
